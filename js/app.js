10ffeecaf655e9c2532465886edb3341d4ccc0d1
pang-wipada <38246310+pangpada@users.noreply.github.com>
2019-10-14 19:22:03 +0700

    update

--- js/app.js
+++ js/app.js
@@ -10,8 +10,8 @@ var firebaseConfig = {
 };
 
 firebase.initializeApp(firebaseConfig);
-
 var db = firebase.firestore(); 
+
 
 document.addEventListener('init', function (event) {
   var page = event.target;
@@ -23,45 +23,16 @@ document.addEventListener('init', function (event) {
       $("#sidemenu")[0].open();
     });
 
-    $("#carousel").empty();
-    db.collection("recomended").get().then((querySnapshot) => {
-      querySnapshot.forEach((doc) => {       
-        var item = `<ons-carousel-item modifier="nodivider" id="item${doc.data().id}" class="recomended_item">
-            <div class="thumbnail" style="background-image: url('${doc.data().photoUrl}')">
-            </div>
-            <div class="recomended_item_title" id="item1_${doc.data().id}">${doc.data().name}</div>
-        </ons-carousel-item>`
-        $("#carousel").append(item);
-      });
+$("#carousel").empty();
+db.collection("recommended").get().then((querySnapshot) => {
+  querySnapshot.forEach((doc) => {       
+    var item = `<ons-carousel-item modifier="nodivider" id="item${doc.data().id}" class="recomended_item">
+        <div class="thumbnail" style="background-image: url('${doc.data().photoUrl}')">
+        </div>
+        <div class="recomended_item_title" id="item1_${doc.data().id}">${doc.data().name}</div>
+    </ons-carousel-item>`
+    $("#carousel").append(item);
+   });
     });
   }
 });
-
-
-/////////////////////////////////////////////////////////////////////////////////
-
-var createAlertDialog = function() {
-    var dialog = document.getElementById('my-alert-dialog');
-  
-    if (dialog) {
-      dialog.show();
-    } else {
-      ons.createElement('alert-dialog.html', { append: true })
-        .then(function(dialog) {
-          dialog.show();
-        });
-    }
-  };
-  
-  var hideAlertDialog = function() {
-    document
-      .getElementById('my-alert-dialog')
-      .hide();
-  };
-
-  var hideAlertDialog1 = function() {
-    document
-      .getElementById('my-alert-dialog')
-      .hide();
-  };
-
