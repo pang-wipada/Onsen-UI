547e5a9d657e9cb8d8d82257f387a35628b9c81b
pang-wipada <38246310+pangpada@users.noreply.github.com>
2019-10-15 13:23:37 +0700

    edit

--- js/app.js
+++ js/app.js
@@ -10,30 +10,19 @@ var firebaseConfig = {
 };
 
 firebase.initializeApp(firebaseConfig);
-var db = firebase.firestore(); 
 
+var db = firebase.firestore();
 
-document.addEventListener('init', function (event) {
-  var page = event.target;
-
-  if (page.id === 'homePage') {
-    console.log("homePage");
-
-    $("#menubtn").click(function () {
-      $("#sidemenu")[0].open();
+    $("#carousel").empty();
+    db.collection("recommended").get().then((querySnapshot) => {
+      querySnapshot.forEach((doc) => {       
+        var item = `<ons-carousel-item modifier="nodivider" id="item${doc.data().id}" class="recomended_item">
+            <div class="thumbnail" style="background-image: url('${doc.data().photoUrl}')">
+            </div>
+        </ons-carousel-item>`
+        $("#carousel").append(item);
+      });
     });
-
-$("#carousel").empty();
-db.collection("recommended").get().then((querySnapshot) => {
-  querySnapshot.forEach((doc) => {       
-    var item = `<ons-carousel-item modifier="nodivider" id="item${doc.data().id}" class="recomended_item">
-        <div class="thumbnail" style="background-image: url('${doc.data().photoUrl}')">
-        </div>
-    </ons-carousel-item>`
-    $("#carousel").append(item);
-   });
-    });
-  }
 
   $("#category").empty();
   db.collection("category").get().then((querySnapshot) => {
@@ -46,4 +35,3 @@ db.collection("recommended").get().then((querySnapshot) => {
    });
     });
     
-});
